4aef29ded4f63d2e938f6487840ed233c9d7b71e
studyge <studygem0194@gmail.com>
2026-06-24 20:56:44 +0000

    feat: add mobile ICT chart and bar replay web app

---
+++ web/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "ICT NSE Chart",
+  description: "ICT / SMC chart and bar replay"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
